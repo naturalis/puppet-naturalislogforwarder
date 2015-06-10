c93c5af8e88ee4559d1c4087a4a2b8ae84c7c9fe
Atze de Vries <atze.devries@naturalis.nl>
2015-06-10 14:28:41 +0200

    added receiver class

---
+++ manifests/receiver.pp
@@ -0,0 +1,44 @@
+#
+#
+#
+class naturalislogforwarder::receiver(
+
+    $redis_password,
+    $redis_servers,
+
+    $ssl_certificate,
+    $ssl_key,
+
+    $logstash_filter = 'filter {}',
+    $incomming_port = 12345,
+
+    $package_url  = 'http://download.elastic.co/logstash/logstash/packages/debian/logstash_1.4.3-1-7e387fb_all.deb'
+
+){
+
+  class { 'logstash':
+    package_url => $package_url,
+  }
+
+  file {'/etc/logstash/keys/lumberjack.crt':
+    ensure  => present,
+    content => $ssl_certificate,
+    require => Class['logstash'],
+    notify  => Service['logstash'],
+  }
+
+  file {'/etc/logstash/keys/lumberjack.key':
+    ensure  => present,
+    content => $ssl_key,
+    require => Class['logstash'],
+    notify  => Service['logstash'],
+  }
+
+  file {'/etc/logstash/conf.d':
+    ensure  => present,
+    content => template('naturalislogforwarder/logstash.conf.erb'),
+    require => Class['logstash'],
+    notify  => Service['logstash'],
+  }
+
+}
