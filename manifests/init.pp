c3af1e3d3e99e85c13e60f310ec2cba583286ae0
Atze de Vries <atze.devries@naturalis.nl>
2015-06-12 14:16:47 +0200

    cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,8 @@
-# == Class: naturalislogforwarder
-#
-# Full description of class naturalislogforwarder here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the function of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'naturalislogforwarder':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
+# Atze de Vries <atze.devries@naturalis.nl>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Naturalis, unless otherwise noted.
 #
 class naturalislogforwarder (
 
@@ -61,13 +30,6 @@ class naturalislogforwarder (
 
 ){
 
-  # file {'/etc/logstashforwarder':
-  #   ensure => directory,
-  # }
-
-  #  $receiver_dns = 'logstash-receiver.naturalis.nl',
-  #  $receiver_port = 12345,
-  #  $receiver_ip = '127.0.0.1',
 
   file {'/etc/logstashforwarder/receiver.crt':
     ensure  => present,
@@ -77,19 +39,9 @@ class naturalislogforwarder (
 
   class { 'logstashforwarder':
     servers     => $lumberjack_hosts,
-    #servers     => ["${receiver_dns}:${receiver_port}"],
     ssl_ca      => '/etc/logstashforwarder/receiver.crt',
-  #  require     => File['/etc/logstashforwarder/receiver.crt'],
     package_url => $package_url,
   }
-
-  # host { $hosts :
-  #   before => Class['logstashforwarder'],
-  # }
-  # host { $receiver_dns :
-  #   ip     => $receiver_ip,
-  #   before => Class['logstashforwarder'],
-  # }
 
   create_resources(host, $hosts, {})
   create_resources(logstashforwarder::file, $file_input_hash,{})
